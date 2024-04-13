43e8ec4f83ee1d28dc26d25259a5e79b087ba728
Leo Sperandio <leomartins64@yahoo.com.br>
2024-04-13 15:12:41 -0300

    feat: config auth

---
+++ src/services/auth/index.ts
@@ -0,0 +1,36 @@
+import NextAuth from 'next-auth'
+import EmailProvider from 'next-auth/providers/nodemailer'
+
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import { prisma } from '../database'
+// import { createStripeCustomer } from '../stripe'
+
+export const {
+  handlers: { GET, POST },
+  auth,
+} = NextAuth({
+  pages: {
+    signIn: '/auth',
+    signOut: '/auth',
+    error: '/auth',
+    verifyRequest: '/auth',
+    newUser: '/app',
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+  secret: process.env.NEXT_PUBLIC_SECRET
+
+  // events: {
+  //   createUser: async (message) => {
+  //     await createStripeCustomer({
+  //       name: message.user.name as string,
+  //       email: message.user.email as string,
+  //     })
+  //   },
+  // },
+})
